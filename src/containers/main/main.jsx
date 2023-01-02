ce24e636f98f72f5bea073d85cd93a7f7f003caf
george Swift <george.swift@nology.io>
2023-01-02 15:15:49 +0000

    further styling changes for better user feedback

--- src/containers/main/main.jsx
+++ src/containers/main/main.jsx
@@ -2,7 +2,7 @@ import "./main.scss";
 import Header from "../header/Header";
 import TaskBar from "../TaskBar/TaskBar";
 import ToDoListDisplay from "../ToDoListDisplay/ToDoListDisplay";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Main = () => {
   const getTaskInput = (event) => {
